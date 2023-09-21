d10e7bf6ce2ab9155f87b7fbff4f5d07465d5f19
Benjilo12 <benjamindarteyy@gmail.com>
2023-09-21 22:39:33 +0100

    bootstrap website

---
+++ index.js
@@ -0,0 +1,13 @@
+const sr = ScrollReveal({
+    distance: '40px',
+    duration: 2050,
+    delay: 200,
+    reset:true
+});
+
+sr.reveal('#hero-text, #hero, #enroll, #img',{origin: 'top'});
+sr.reveal('#news, #input',{origin: 'top'});
+sr.reveal('#card',{origin: 'top'});
+sr.reveal('#mentals, #react',{origin: 'right'});
+sr.reveal('#questions',{origin: 'left'});
+sr.reveal('#instructors , #contact',{origin: 'top'});
